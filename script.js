89150975d1209bd93052db513bc8a3a741d5bc19
OXYGEN1991 <131229042+OXYGEN1991@users.noreply.github.com>
2024-09-17 19:22:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Designer", "Freelancer", "Social Media Manager"],
+        strings: [ "Illustrator","Graphic Designer", "Freelancer", "Virtual Assistant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Designer", "Freelancer", "Social Media Manager"],
+        strings: [ "Illustrator","Graphic Designer", "Freelancer", "Virtual Assistant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
